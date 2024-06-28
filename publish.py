7301f90f381792a04ab7541070ddaf91f3eb10e1
出分寛人 <debunhiroto@debunhirotononotobukkukonpyuta.local>
2024-06-28 12:24:10 +0900

    発行プログラムをオプション引数で実装

--- publish.py
+++ publish.py
@@ -2,6 +2,7 @@ import binascii
 import nfc
 import os
 import secrets
+import argparse
 
 import config
 import gen_mac
@@ -15,22 +16,28 @@ class MyCardReader(object):
         sc_read = nfc.tag.tt3.ServiceCode(0, 0x0b)
         sc_write = nfc.tag.tt3.ServiceCode(0, 0x09)
 
-        ID_six = os.urandom(6) # かぶる可能性あるかも
-        ID = b"\x00"*10 + ID_six # write_without_encryptionに使います。
-        bc82 = nfc.tag.tt3.BlockCode(0x82, service=0)
-        tag.write_without_encryption([sc_write], [bc82], ID)
+        if args.id:
+            ID_six = os.urandom(6) # かぶる可能性あるかも
+            ID = b"\x00"*10 + ID_six # write_without_encryptionに使います。
+            bc82 = nfc.tag.tt3.BlockCode(0x82, service=0)
+            tag.write_without_encryption([sc_write], [bc82], ID)
+        else:
+            bc82 = nfc.tag.tt3.BlockCode(0x82, service=0)
+            ID_six = tag.read_without_encryption([sc_read], [bc82])[-6:]
 
-        print(bytes(tag.idm) + b"\x00\x00" + ID_six)
+        if args.ck:
+            CK = gen_cardkey.gen_key(bytes(tag.idm) + b"\x00\x00" + ID_six, self.master) # IDmとIDdは同じです。
+            bc87 = nfc.tag.tt3.BlockCode(0x87, service=0)
+            tag.write_without_encryption([sc_write], [bc87], CK)
 
-        CK = gen_cardkey.gen_key(bytes(tag.idm) + b"\x00\x00" + ID_six, self.master) # IDmとIDdは同じです。
-        bc87 = nfc.tag.tt3.BlockCode(0x87, service=0)
-        tag.write_without_encryption([sc_write], [bc87], CK)
+            CKV = b"\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+            bc86 = nfc.tag.tt3.BlockCode(0x86, service=0)
+            tag.write_without_encryption([sc_write], [bc86], CKV)
 
-        CKV = b"\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-        bc86 = nfc.tag.tt3.BlockCode(0x86, service=0)
-        tag.write_without_encryption([sc_write], [bc86], CKV)
-
-        MC = b"\xFF\xFF\xFF\x01\x07\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        if args.mc:
+            MC = bytes.fromhex(args.mc)
+        else:
+            MC = b"\xFF\xFF\x00\x01\x07\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
         bc88 = nfc.tag.tt3.BlockCode(0x88, service=0)
         tag.write_without_encryption([sc_write], [bc88], MC)
 
@@ -40,15 +47,18 @@ class MyCardReader(object):
         sc_read = nfc.tag.tt3.ServiceCode(0, 0x0b)
         sc_write = nfc.tag.tt3.ServiceCode(0, 0x09)
 
-        print("Enter userID\n")
-        user = input().encode()
-        while(len(user) > 16):
-            print("userID must be < 16\n")
-            user = input().encode()
-        bc00 = nfc.tag.tt3.BlockCode(0x00, service=0)
-        tag.write_without_encryption([sc_write], [bc00], user)
+        if args.user:      
+            user = args.user.encode()
+            if len(user) > 16:
+                print('userID must be < 16')
+            else:
+                bc00 = nfc.tag.tt3.BlockCode(0x00, service=0)
+                tag.write_without_encryption([sc_write], [bc00], user)
         
-        MC = b"\xFF\xFF\xFF\x01\x07\x01\xFF\xFF\xFF\xFF\x00\x00\x00\x00\x00\x00"
+        if args.mc:
+            MC = bytes.fromhex(args.mc)
+        else:
+            MC = b"\xFF\xFF\xFF\x01\x07\x01\xFF\xFF\xFF\xFF\x00\x00\x00\x00\x00\x00"
         bc88 = nfc.tag.tt3.BlockCode(0x88, service=0)
         tag.write_without_encryption([sc_write], [bc88], MC)
         
@@ -56,16 +66,25 @@ class MyCardReader(object):
 
     def read_id(self):
         clf = nfc.ContactlessFrontend('usb')
-        try:
-            clf.connect(rdwr={'on-connect': self.on_connect_1})
-        finally:
-            clf.close()
-        try:
-            clf.connect(rdwr={'on-connect': self.on_connect_2})
-        finally:
-            clf.close()
+        if args.stage == 1:
+            try:
+                clf.connect(rdwr={'on-connect': self.on_connect_1})
+            finally:
+                clf.close()
+        if args.stage == 2:
+            try:
+                clf.connect(rdwr={'on-connect': self.on_connect_2})
+            finally:
+                clf.close()
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='this program supports publishing felica lite S cards')
+    parser.add_argument('stage', help='the stage of issuance you want to perform (1or2)', choices=[1, 2])
+    parser.add_argument('--id', help='If specified, ID will not be changed (at 1st issuance)', action='store_false')
+    parser.add_argument('--ck', help='If specified, CK will not be changed (at 1st issuance)', action='store_false')
+    parser.add_argument('--user', help='the user name to be stored in S_PAD0 (16 characters or less)')
+    parser.add_argument('--mc', help='specify commands to be stored in MC', type=str)
+    args = parser.parse_args()
     cr = MyCardReader()
     while True:
         cr.read_id()
